c1a95d55160c9de70acae30830048d1166bfc69b
Alexey Prikhodko <48862700+YAYAYru@users.noreply.github.com>
2021-11-09 19:09:08 +0700

    Fix structure folders

---
+++ code/first_tv_camera.py
@@ -0,0 +1,35 @@
+from first_tv_module import mediapipe_detection, draw_styled_landmarks, mp_holistic, mp_drawing, extract_keypoints
+
+import cv2
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+import time
+import mediapipe as mp
+
+
+cap = cv2.VideoCapture(0)
+# Set mediapipe model 
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
+
+        # Read feed
+        ret, frame = cap.read()
+
+        # Make detections
+        image, results = mediapipe_detection(frame, holistic)
+        #print(results)
+        
+        # Draw landmarks
+        draw_styled_landmarks(image, results)
+
+        # Show to screen
+        cv2.imshow('OpenCV Feed', image)
+
+        # Break gracefully
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
---
+++ code/first_tv_folder.py
@@ -0,0 +1,10 @@
+from first_tv_module import actions, DATA_PATH, no_sequences
+import os
+
+for action in actions: 
+    for sequence in range(no_sequences):
+        try: 
+            os.makedirs(os.path.join(DATA_PATH, action, str(sequence)))
+        except:
+            pass
+
